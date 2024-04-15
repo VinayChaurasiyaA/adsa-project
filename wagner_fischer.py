557758402b9dd5e9cf683b0613e4eba5b0e49116
vinay <vinaychaurasiya73@gmail.com>
2024-04-15 19:29:10 +0530

    d

--- wagner_fischer.py
+++ wagner_fischer.py
@@ -129,8 +129,8 @@ def spell_check_handler():
 CORS(app)  # Enable CORS for all routes
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=8080)
     app.run(debug=True)
 # from flask import Flask, render_template, request, jsonify
 # from flask_cors import CORS
